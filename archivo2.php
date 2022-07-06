575743c1af46535b31c0574640c5416e601d6f9d
dulce <106275114+DulceYuridia@users.noreply.github.com>
2022-07-06 02:40:08 -0500

    Unidad III. Tarea 2

---
+++ archivo2.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+        $x = 20;
+        $y = 8;
+        
+        echo  $x + $y;
+        
+        echo "<br>"
+        ?> 
+        
+        <?php
+        $x = 9.8;
+        $y = 7;
+        
+        echo  $x + $y;
+        
+        echo "<br>"
+        ?> 
+        
+        <?php
+        $x = 9;
+        $y = 7;
+        
+        echo  $x + $y;
+        
+        echo "<br>"
+        ?> 
+
+        <?php
+        $var1 = 'Dulce';
+        $var2 = ' Chavarria';
+        $texto_completo = $var1 . $var2;
+        echo $texto_completo;
+        ?>
+
+        </body>
+</html>
