4bc0f131313e015af32d83ec7972a6973d3c0b68
dulce <106275114+DulceYuridia@users.noreply.github.com>
2022-07-06 02:38:15 -0500

    unidad III. Tarea1

---
+++ archivo1.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+        echo "Dulce Yuridia Chavarria Hernandez<br>";
+        echo "Tercer cuatrimestre<br>";
+        ?> 
+        </body>
+</html>
